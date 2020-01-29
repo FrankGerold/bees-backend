f5c06e5d8ff74c760dca25441054ff5b988bdc3e
Frank Gerold <FrankETSGerold@gmail.com>
2020-01-29 17:02:20 -0500

    Following jwt lab for auth. Create creation conrol and Route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def create
+
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: #(serialize user here), status: created
+    else
+      render json: { error: 'User Creation Failed'}, status: not_acceptable
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:userName, :password, :bio, :photo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions
-  resources :quizzes
-  resources :users
+  # resources :answers
+  # resources :questions
+  # resources :quizzes
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
