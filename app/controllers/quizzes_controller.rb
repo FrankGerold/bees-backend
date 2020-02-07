2f65d634f3bc92423e948dbc0c02072b15e70929
Frank Gerold <FrankETSGerold@gmail.com>
2020-02-06 20:12:22 -0500

    setting up more controllers for building out quizzes tomorrow

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,24 @@
 class QuizzesController < ApplicationController
+  skip_before_action :authorized
 
-  
+# TEsting
+def index
+  @quizzes = Quiz.all
+  # render json: @quizzes
+   render json:  QuizSerializer.new(@quizzes)
+end
+
+def show
+  @quiz = Quiz.find (params[:id])
+  render json:  QuizSerializer.new(@quiz)
+end
+# end test
+
+  def create
+
+
+
+  end
+
 
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,6 +1,5 @@
 class QuestionSerializer
   include FastJsonapi::ObjectSerializer
   attributes :topic, :question, :full_answer
-  has_many :questions_quizzes
   has_many :answers
 end
