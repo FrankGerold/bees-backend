4ffeb321ec7586f8b20c88179b6e3cd24726a539
Frank Gerold <FrankETSGerold@gmail.com>
2020-02-09 14:11:08 -0500

    Set up some quiz actions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,24 +1,52 @@
 class QuizzesController < ApplicationController
-  skip_before_action :authorized
+  skip_before_action :authorized, only: [:index, :show, :update] #Skipping auth for testing methods
 
-# TEsting
-def index
-  @quizzes = Quiz.all
-  # render json: @quizzes
-   render json:  QuizSerializer.new(@quizzes)
-end
+  # TEsting
+  def index
+    @quizzes = Quiz.all
+    # render json: @quizzes
+     render json:  QuizSerializer.new(@quizzes)
+  end
 
-def show
-  @quiz = Quiz.find (params[:id])
-  render json:  QuizSerializer.new(@quiz)
-end
-# end test
+  def show
+    @quiz = Quiz.find (params[:id])
+    render json:  QuizSerializer.new(@quiz)
+  end
+  # end test
 
   def create
+    @quiz = Quiz.new(quiz_params)
+    @quiz.user = current_user
+    @quiz.save
 
+    generate_questions(@quiz)
 
+    render json: QuizSerializer.new(@quiz)
+  end
+
+  def user
+    @user = current_user
+    @quizzes = @user.quizzes
+
+    render json: QuizSerializer.new(@quizzes)
+  end
+
+  def update
+
+  private
+
+  def generate_questions(quiz)
+
+    randomQuestions = Question.order('RANDOM()').take(10)
+
+    for question in randomQuestions do
+      QuestionsQuiz.create(quiz: quiz, question: question)
+    end
 
   end
 
+  def quiz_params
+    params.permit(:topic, :name, :score)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  # resources :answers
+  resources :users, only: [:create, :show, :index]
+    post '/login', to: 'auth#create'
+    get '/profile', to: 'users#profile'
 
-  resources :quizzes, only:[:create, :show, :index]
+  resources :quizzes, only:[:create, :show, :index, :update]
+    get '/userquizzes', to: 'quizzes#user'
 
   resources :questions, only:[:create, :show, :index]
 
-  resources :users, only: [:create, :show, :index]
+  # resources :answers
 
-  post '/login', to: 'auth#create'
-
-  get '/profile', to: 'users#profile'
-
-  get '/auth', to: 'auth#persist'
+  # get '/auth', to: 'auth#persist'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
